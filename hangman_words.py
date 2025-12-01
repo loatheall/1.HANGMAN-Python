372b360ada7ac62b26ba114e5519d17a3a30d5ac
loatheall <140192482+loatheall@users.noreply.github.com>
2025-12-02 00:56:47 +0100

    Create hangman_words.py

---
+++ hangman_words.py
@@ -0,0 +1,19 @@
+words = [
+    "address", "algorithm", "append", "array", "binary", "boolean",
+    "break", "bubble", "cache", "capitalize", "class", "clear",
+    "close", "compiler", "condition", "continue", "copy", "count",
+    "cycle", "decimal", "debugger", "dictionary", "edge", "endswith",
+    "encapsulation", "enumerate", "exception", "file", "filter",
+    "find", "float", "format", "function", "graph", "hangman",
+    "hexadecimal", "inheritance", "input", "insert", "integer",
+    "interpreter", "join", "keyboard", "lambda", "library", "list",
+    "local", "loop", "looping", "lower", "map", "matrix", "memory",
+    "merge", "module", "monitor", "nested", "node", "object", "open",
+    "operator", "output", "package", "pass", "path", "pointer",
+    "pop", "process", "programming", "python", "queue", "quick",
+    "range", "read", "recursion", "reduce", "register", "replace",
+    "return", "reverse", "scope", "script", "search", "segment",
+    "selection", "set", "slice", "sort", "split", "stack",
+    "statement", "startswith", "string", "syntax", "thread", "tree",
+    "tuple", "upper", "variable", "vector", "write", "zip"
+]
